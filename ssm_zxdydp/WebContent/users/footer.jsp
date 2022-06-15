54634608b1910c8648b7aafc546f41d67f984cbf
Theshy <1472063612@qq.com>
2022-06-15 22:42:28 +0800

    “提交文件”

---
+++ ssm_zxdydp/WebContent/users/footer.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
+%>
+
+<div class="all-help bgc-f2">
+	<div class="container clearfix" style="overflow: hidden;">
+	</div>
+</div>
+<div class="all-footer">
+	<div class="container clearfix">
+		<p class="p-icp">
+			<a href="admin/index.jsp" target="_blank">管理员入口</a>
+		</p>
+	</div>
+</div>
---
+++ ssm_zxdydp/src/com/entity/Film.java
@@ -0,0 +1,142 @@
+package com.entity;
+
+import com.util.VeDate;
+
+public class Film {
+	private String filmid = "F" + VeDate.getStringId();
+	private String filmname;
+	private String image;
+	private String cateid;
+	private String price;
+	private String recommend;
+	private String thestart;
+	private String theend;
+	private String hits;
+	private String sellnum;
+	private String contents;
+	private String catename;
+
+	private String daoyan;
+	private String zhuyan;
+	private String shichang;
+
+	public String getFilmid() {
+		return filmid;
+	}
+
+	public void setFilmid(String filmid) {
+		this.filmid = filmid;
+	}
+
+	public String getFilmname() {
+		return this.filmname;
+	}
+
+	public void setFilmname(String filmname) {
+		this.filmname = filmname;
+	}
+
+	public String getImage() {
+		return this.image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getCateid() {
+		return this.cateid;
+	}
+
+	public void setCateid(String cateid) {
+		this.cateid = cateid;
+	}
+
+	public String getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getRecommend() {
+		return this.recommend;
+	}
+
+	public void setRecommend(String recommend) {
+		this.recommend = recommend;
+	}
+
+	public String getThestart() {
+		return this.thestart;
+	}
+
+	public void setThestart(String thestart) {
+		this.thestart = thestart;
+	}
+
+	public String getTheend() {
+		return this.theend;
+	}
+
+	public void setTheend(String theend) {
+		this.theend = theend;
+	}
+
+	public String getHits() {
+		return this.hits;
+	}
+
+	public void setHits(String hits) {
+		this.hits = hits;
+	}
+
+	public String getSellnum() {
+		return this.sellnum;
+	}
+
+	public void setSellnum(String sellnum) {
+		this.sellnum = sellnum;
+	}
+
+	public String getContents() {
+		return this.contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getCatename() {
+		return this.catename;
+	}
+
+	public void setCatename(String catename) {
+		this.catename = catename;
+	}
+
+	public String getDaoyan() {
+		return daoyan;
+	}
+
+	public void setDaoyan(String daoyan) {
+		this.daoyan = daoyan;
+	}
+
+	public String getZhuyan() {
+		return zhuyan;
+	}
+
+	public void setZhuyan(String zhuyan) {
+		this.zhuyan = zhuyan;
+	}
+
+	public String getShichang() {
+		return shichang;
+	}
+
+	public void setShichang(String shichang) {
+		this.shichang = shichang;
+	}
+}
